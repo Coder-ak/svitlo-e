109c13ebfbd63516346b5433979cf365dd85ba37
Aleksey Kirpichnikov <akirpichnikov@gbsfo.com>
2023-02-04 13:39:03 +0200

    add current interval diff time

--- front/script.js
+++ front/script.js
@@ -14,6 +14,7 @@ function getLampIcon(status) {
 }
 
 function secondsToTime(totalSeconds) {
+  totalSeconds = totalSeconds / 1000;
   let hours = Math.floor(totalSeconds / 3600);
   totalSeconds %= 3600;
   let minutes = Math.floor(totalSeconds / 60);
@@ -29,7 +30,12 @@ async function init() {
   const textShort = getStatus(light).charAt(0).toUpperCase() + getStatus(light).slice(1);
   const textFull = `З ${formatDate(timestamp)} ${getStatus(light)}`;
 
+  const diff = new Date().getTime() - timestamp;
+  console.log(new Date().getTime(), timestamp);
+  const textDiff = `Світ ${light ? 'є' : 'відсутній'} ${secondsToTime(diff)}`
+
   document.getElementById('content').innerText = textFull;
+  document.getElementById('vidkl').innerText = textDiff;
 
   document.title = textShort;
   document.querySelectorAll('meta[property=og\\:title]')[0].setAttribute('content', textShort)
@@ -46,7 +52,7 @@ async function stats() {
     const diff = item.timestamp - data[index + 1]?.timestamp || 0;
 
     return `<div class="grid ${item.light ? 'on' : 'off'}">
-      <img src="assets/lamp_${item.light ? 'on' : 'off'}.svg" title="${item.light ? 'Увімкнено' : 'Вимкнено'}" class="icon"/> <div class="time">${formatDate(item.timestamp, true)}</div> <div class="diff ${item.light ? 'off' : 'on'}">${secondsToTime(diff/1000)}</div></div>`
+      <img src="assets/lamp_${item.light ? 'on' : 'off'}.svg" title="${item.light ? 'Увімкнено' : 'Вимкнено'}" class="icon"/> <div class="time">${formatDate(item.timestamp, true)}</div> <div class="diff ${item.light ? 'off' : 'on'}">[${secondsToTime(diff)}]</div></div>`
   });
 
   document.getElementById('stats').innerHTML = table.join('\n');
