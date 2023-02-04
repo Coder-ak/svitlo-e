519252ead73022d2bd51aa853ce524575488df9c
Aleksey Kirpichnikov <akirpichnikov@gbsfo.com>
2023-02-04 13:39:47 +0200

    fix layout

--- front/script.js
+++ front/script.js
@@ -20,7 +20,7 @@ function secondsToTime(totalSeconds) {
   let minutes = Math.floor(totalSeconds / 60);
   let seconds = Math.floor(totalSeconds % 60);
 
-  return "[" + String(hours).padStart(2, "0") + ":" + String(minutes).padStart(2, "0") + ":" + String(seconds).padStart(2, "0") + "]";
+  return String(hours).padStart(2, "0") + ":" + String(minutes).padStart(2, "0") + ":" + String(seconds).padStart(2, "0");
 }
 
 async function init() {
