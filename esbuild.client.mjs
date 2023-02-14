a326cf64009cd55d50f1253181b75c2bff4ab078
Aleksey Kirpichnikov <akirpichnikov@gbsfo.com>
2023-02-14 22:32:59 +0200

    Brand new style

--- esbuild.client.mjs
+++ esbuild.client.mjs
@@ -1,11 +1,12 @@
 import esbuild from 'esbuild';
 import { livereloadPlugin } from '@jgoz/esbuild-plugin-livereload';
+import { lessLoader } from 'esbuild-plugin-less';
 
 const watch = process.argv.includes('--watch');
 
 const buildOptions = {
-  entryPoints: ['./src/client/script.ts'],
-  outfile: 'dist/client/script.js',
+  entryPoints: ['src/client/script.ts', 'src/client/style.less'],
+  outdir: 'dist/client',
   bundle: true,
   minify: !watch,
   sourcemap: watch,
@@ -14,9 +15,10 @@ const buildOptions = {
 };
 
 if (watch) {
-  buildOptions.plugins = [livereloadPlugin()];
+  buildOptions.plugins = [livereloadPlugin(), lessLoader()];
   const ctx = await esbuild.context(buildOptions);
   await ctx.watch();
 } else {
+  buildOptions.plugins = [lessLoader()];
   await esbuild.build(buildOptions);
 }
