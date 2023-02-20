ba0128a1521f9edf901483db657daf21f6dc2408
Aleksey Kirpichnikov <akirpichnikov@gbsfo.com>
2023-02-20 18:50:00 +0200

    add htmlLoader esbuild plugin

--- esbuild.client.mjs
+++ esbuild.client.mjs
@@ -1,11 +1,13 @@
 import esbuild from 'esbuild';
 import { livereloadPlugin } from '@jgoz/esbuild-plugin-livereload';
 import { lessLoader } from 'esbuild-plugin-less';
+import { htmlPlugin } from '@craftamap/esbuild-plugin-html';
 
 const watch = process.argv.includes('--watch');
 
 const buildOptions = {
   entryPoints: ['src/client/script.ts', 'src/client/style.less'],
+  entryNames: '[name]-[hash]',
   outdir: 'dist/client',
   bundle: true,
   minify: !watch,
@@ -14,13 +16,24 @@ const buildOptions = {
   logLevel: 'info',
   write: true,
   metafile: true,
+  plugins: [
+    lessLoader(),
+    htmlPlugin({
+      files: [
+        {
+          entryPoints: ['src/client/script.ts', 'src/client/style.less'],
+          filename: 'index.html',
+          htmlTemplate: 'src/client/index.html',
+        },
+      ],
+    }),
+  ],
 };
 
 if (watch) {
-  buildOptions.plugins = [livereloadPlugin(), lessLoader()];
+  buildOptions.plugins = [...buildOptions.plugins, livereloadPlugin()];
   const ctx = await esbuild.context(buildOptions);
   await ctx.watch();
 } else {
-  buildOptions.plugins = [lessLoader()];
   await esbuild.build(buildOptions);
 }
